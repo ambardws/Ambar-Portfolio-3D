f4787717f9f3fbec0c9ca346acac2634366cc1e3
Ambar Dwi Saputra <ambardwisaputra@gmail.com>
2023-12-19 14:04:45 +0700

    style:  change hero asset

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,17 +2,14 @@ import React from "react";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
+import rocket from "../assets/rocket.png";
 
 const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-col sm:flex-row items-start gap-5`}
       >
-        <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915eff]"></div>
-          <div className="w-1 sm:h-80 h-40 violet-gradient"></div>
-        </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className="text-[#915eff]">Ambar</span>
@@ -21,9 +18,18 @@ const Hero = () => {
             I develop 3D visual, user <br className="sm:block hidden" />{" "}
             interfaces and web applications
           </p>
+          <button className="mt-3 bg-[#915eff] px-2 py-3 rounded-xl shadow-primary cursor-pointer hover:bg-tertiary font-medium">
+            <a href="#projects">See Project</a>
+          </button>
         </div>
+
+        <img
+          src={rocket}
+          alt="rocket"
+          className="h-72 sm:h-96 mx-auto my-auto"
+        />
       </div>
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">

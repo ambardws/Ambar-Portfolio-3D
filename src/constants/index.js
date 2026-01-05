579fcbf1a1ffacbb7363f8ca112eac0ec281e5a0
Ambar Dwi Saputra <ambardwisaputra@gmail.com>
2026-01-05 13:31:41 +0700

    update experience and testimonial images

--- src/constants/index.js
+++ src/constants/index.js
@@ -111,7 +111,7 @@ const experiences = [
   {
     title: "Frontend Developer",
     company_name: "Ako Media Asia",
-    icon: 'https://media.licdn.com/dms/image/v2/D560BAQH-lIDHavg74g/company-logo_100_100/company-logo_100_100/0/1708583074448/salt_indonesia_logo?e=1762992000&v=beta&t=08h44Dd-1arwQ9yqzmahDULcvbwj_Ac6Wwc5LIJrO68',
+    icon: 'https://media.licdn.com/dms/image/v2/D560BAQH-lIDHavg74g/company-logo_100_100/company-logo_100_100/0/1708583074448/salt_indonesia_logo?e=1769040000&v=beta&t=uqa8OC8qGEclQG_6RKzfDr1mzTW_1YwdLINxh7mKC5g',
     iconBg: "#d20681",
     date: "Oct 2024 - Present",
     points: [
@@ -126,7 +126,7 @@ const experiences = [
   {
     title: "Frontend Developer",
     company_name: "Meteor Inovasi Digital",
-    icon: 'https://media.licdn.com/dms/image/v2/D4D0BAQETuQ4t1c57vA/company-logo_100_100/company-logo_100_100/0/1683189653916/meteorid_logo?e=1762992000&v=beta&t=XZJPXnxhkjBBVsddN0hKfKN88B4FbcvaHdt_o6oB6vM',
+    icon: 'https://media.licdn.com/dms/image/v2/D4D0BAQETuQ4t1c57vA/company-logo_100_100/company-logo_100_100/0/1683189653916/meteorid_logo?e=1769040000&v=beta&t=xparxc5gCFcQRxivQoTRZMTao1S53K_h5dNG620O_S4',
     iconBg: "#FE0000",
     date: "Feb 2022 - Dec 2023",
     points: [
@@ -139,7 +139,7 @@ const experiences = [
   {
     title: "Frontend Developer",
     company_name: "Axiata Digital Labs",
-    icon: 'https://media.licdn.com/dms/image/v2/C560BAQE9-4bSDMJ8mg/company-logo_100_100/company-logo_100_100/0/1631153821451/axiatadigitallabsatthelab_logo?e=1762992000&v=beta&t=_0cSyGiM-Hg2-ynN9c60NQpV_33ORfRr2TVMVLj1YSg',
+    icon: 'https://media.licdn.com/dms/image/v2/C560BAQE9-4bSDMJ8mg/company-logo_100_100/company-logo_100_100/0/1631153821451/axiatadigitallabsatthelab_logo?e=1769040000&v=beta&t=hzM1dwlvj1v3CC6dzcMbHzpcWmO5E9xgDba639sBcxo',
     iconBg: "#FFF",
     date: "Apr 2022 - Jun 2023",
     points: [
@@ -151,7 +151,7 @@ const experiences = [
   {
     title: "Internship Full Stack Developer",
     company_name: "Uasoft",
-    icon: 'https://media.licdn.com/dms/image/v2/C560BAQERZfLHJSABOw/company-logo_100_100/company-logo_100_100/0/1630654277581/uatech_logo?e=1762992000&v=beta&t=oGWK29uz4uiq0Cor81noKmYkb5MmVXV6aPQ1wBVDwIM',
+    icon: 'https://media.licdn.com/dms/image/v2/C560BAQERZfLHJSABOw/company-logo_100_100/company-logo_100_100/0/1630654277581/uatech_logo?e=1769040000&v=beta&t=H0tcskphmFeeKQNbn0QwtnN-PkgtdCUBTEGictvXB_I',
     iconBg: "#000",
     date: "Aug 2020 - Oct 2020",
     points: [
@@ -171,7 +171,7 @@ const testimonials = [
     name: "Ryan Apriansyah",
     designation: "Senior Frontend Developer",
     company: "Ako Media Asia",
-    image: "https://randomuser.me/api/portraits/men/4.jpg",
+    image: "https://media.licdn.com/dms/image/v2/C5603AQEnXZiwP6AGAg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1646544454796?e=1769040000&v=beta&t=4lX05637ib8y8ujrfbwd42cZsnmCbJoez9Mh5KNTovQ",
   },
   {
     testimonial:
@@ -179,15 +179,15 @@ const testimonials = [
     name: "Jodhi Gunawan",
     designation: "Frontend Developer",
     company: "Freelancer Abtract Writer",
-    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    image: "https://media.licdn.com/dms/image/v2/C5103AQE_RTX_YmbmOQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1517039601414?e=1769040000&v=beta&t=p0U9f6W58kFR18Em80JDhYqpuxyX76J9JfI9R7xvl6Y",
   },
   {
     testimonial:
-      "He has always been passionate about technology. It's amazing to see how far he's come as a developer.",
-    name: "My Mother",
-    designation: "CEO",
-    company: "Home",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
+      "It's rare to find a web developer who combines technical skill with a true passion for helping clients succeed.",
+    name: "Ledi Hildawan",
+    designation: "Frontend Developer",
+    company: "Ako Media Asia",
+    image: "https://media.licdn.com/dms/image/v2/D5603AQG0e27ZLVWiXw/profile-displayphoto-shrink_200_200/B56ZdE7UYFH8AY-/0/1749208090657?e=1769040000&v=beta&t=ydLuRoVGgql7-x3yICwy1NQYZaGDvSSX5xkCA-GhB8s",
   },
 ];
 
